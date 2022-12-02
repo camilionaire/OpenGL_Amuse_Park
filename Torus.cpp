ad0e5a4580cc1349a149037fbcf2feacd82f2550
Camilo <camilo3@pdx.edu>
2022-12-01 22:35:28 -0800

    transfer commit

--- Torus.cpp
+++ Torus.cpp
@@ -42,41 +42,65 @@ struct Vector3 { float x; float y; float z; };
 void
 Torus::Draw(void) {
 
-	glTranslatef(35, -40, 0.5);
-	glColor3f(1, 0.0627, 0.9412);
+	glPushMatrix();
+	// moves it to the front left and a little off the ground
+//	glTranslatef(35, -40, 1);
+	glColor3f(1, 0.0627, 0.9412); // hot pink
+	glColor3f(1, 1, 1); // white
 
-    for (int d = 0; d <= 8; d++) {
-		glBegin(GL_POLYGON);
-		float angle = d * 45.0 / 360;
-		glNormal3f(cos(angle), sin(angle), 0);
-		// x if zero for all here... i think
-		glVertex3f(0, 4, 0);
-		glVertex3f(0, 5, 0);
-		glVertex3f(0, 6, 1);
-		glVertex3f(0, 5, 2);
-		glVertex3f(0, 4, 2);
-		glVertex3f(0, 3, 1);
-		glEnd();
-		glRotatef(45.0, 0, 0, 1);
-    }
-	// to unrotate it, I think...
-	glRotatef(-45.0, 0, 0, 1);
-	glColor3f(0.1216, 0.3176, 1);
+	glTranslatef(0, 5, 0);
+
+
+	for (int d = 0; d <= 3; d++) {
+	glBegin(GL_TRIANGLE_STRIP);
+		glVertex3f(0, 1, 0);
+		//		glTranslatef(0, 1, 0);
+		glVertex3f(0, 2, 0);
+		//		glTranslatef(0, -1, 1);
+		glVertex3f(0, 1, 1);
+		//		glTranslatef(0, 1, 0);
+		glVertex3f(0, 2, 1);
+	glEnd();
+		glTranslatef(0, 1, 0);
+	}
 	
-    for (int d = 0; d <= 8; d++) {
-		glBegin(GL_POLYGON);
-		float angle = d * 45.0 / 360;
-		glNormal3f(-cos(angle), -sin(angle), 0);
-		// x if zero for all here... i think
-		glVertex3f(0, 3, 1);
-		glVertex3f(0, 4, 2);
-		glVertex3f(0, 5, 2);
-		glVertex3f(0, 6, 1);
-		glVertex3f(0, 5, 0);
-		glVertex3f(0, 4, 0);
-		glEnd();
-		glRotatef(45.0, 0, 0, 1);
-    }
+
+
+	glPopMatrix();
+ 
+//	// this is the hot pink side?
+//    for (int d = 0; d <= 3; d++) {
+//		glBegin(GL_POLYGON);
+//		float angle = d * 45.0 / 360;
+//		glNormal3f(cos(angle), sin(angle), 0);
+//		// x if zero for all here... i think
+//		glVertex3f(0, 4, 0);
+//		glVertex3f(0, 5, 0);
+//		glVertex3f(0, 6, 1);
+//		glVertex3f(0, 5, 2);
+//		glVertex3f(0, 4, 2);
+//		glVertex3f(0, 3, 1);
+//		glEnd();
+//		glRotatef(45.0, 0, 0, 1);
+//    }
+//	// to unrotate it, I think...
+//	glRotatef(-45.0, 0, 0, 1);
+//	glColor3f(0.1216, 0.3176, 1);
+//	
+//    for (int d = 0; d <= 3; d++) {
+//		glBegin(GL_POLYGON);
+//		float angle = d * 45.0 / 360;
+//		glNormal3f(-cos(angle), -sin(angle), 0);
+//		// x if zero for all here... i think
+//		glVertex3f(0, 3, 1);
+//		glVertex3f(0, 4, 2);
+//		glVertex3f(0, 5, 2);
+//		glVertex3f(0, 6, 1);
+//		glVertex3f(0, 5, 0);
+//		glVertex3f(0, 4, 0);
+//		glEnd();
+//		glRotatef(45.0, 0, 0, 1);
+//    }
     
 //    glPushMatrix();
 //    glCallList(display_list);
